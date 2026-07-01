98e3dff71a91e1f8a8642695795b89b79dd07a66
dnjswldnd-3513 <zheld3513@gmail.com>
2026-07-01 17:52:31 +0900

    feat: Goal Entity class 구조 작성

--- src/main/java/com/example/dopamine_detox/domain/Goal.java
+++ src/main/java/com/example/dopamine_detox/domain/Goal.java
@@ -1,4 +1,35 @@
 package com.example.dopamine_detox.domain;
 
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Goal {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY) // 필요할때만 db 조회한다는것
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "appCategory_id")
+    private AppCategory appCategory;
+
+    @Column(nullable = false)
+    private int limitMinutes;
+
+    @Builder
+    public Goal( Member member, AppCategory appCategory,int limitMinutes ) {
+        this.member = member;
+        this.appCategory = appCategory;
+        this.limitMinutes = limitMinutes;
+    }
 }
