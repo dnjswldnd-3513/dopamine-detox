829cad8d2f06b3df296d5d18cff1ecdb21ea338a
dnjswldnd-3513 <zheld3513@gmail.com>
2026-07-01 17:39:23 +0900

    feat: AppCategory Entity class 생성

--- src/main/java/com/example/dopamine_detox/domain/AppCategory.java
+++ src/main/java/com/example/dopamine_detox/domain/AppCategory.java
@@ -1,4 +1,23 @@
 package com.example.dopamine_detox.domain;
 
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class AppCategory {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String name;
+
+    public AppCategory(String name) {
+        this.name = name;
+    }
 }
