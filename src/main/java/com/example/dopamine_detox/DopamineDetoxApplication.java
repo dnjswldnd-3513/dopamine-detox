a04b758a2d21f5537c3c4db7e60b6d7cecb86cac
dnjswldnd-3513 <zheld3513@gmail.com>
2026-07-01 13:24:06 +0900

    chore: 프로젝트 초기 설정

---
+++ src/main/java/com/example/dopamine_detox/DopamineDetoxApplication.java
@@ -0,0 +1,13 @@
+package com.example.dopamine_detox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DopamineDetoxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DopamineDetoxApplication.class, args);
+	}
+
+}
