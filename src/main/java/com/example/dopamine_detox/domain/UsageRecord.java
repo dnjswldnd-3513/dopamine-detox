9abcf3cfcdcee3b07405c21cec23fa2bf2a6292c
dnjswldnd-3513 <zheld3513@gmail.com>
2026-07-01 18:02:50 +0900

    feat: UsageRecord Entity class 구조 생성

--- src/main/java/com/example/dopamine_detox/domain/UsageRecord.java
+++ src/main/java/com/example/dopamine_detox/domain/UsageRecord.java
@@ -1,4 +1,44 @@
 package com.example.dopamine_detox.domain;
 
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class UsageRecord {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "appCategory_id")
+    private AppCategory appCategory;
+
+    @Column(nullable = false)
+    private LocalDateTime startTime;
+    @Column(nullable = false)
+    private LocalDateTime endTime;
+
+    @Column(nullable = false)
+    private int durationMinutes;
+
+    @Builder
+    public UsageRecord(Member member, AppCategory appCategory, LocalDateTime startTime, LocalDateTime endTime, int durationMinutes) {
+        this.member = member;
+        this.appCategory = appCategory;
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.durationMinutes = durationMinutes;
+    }
 }
