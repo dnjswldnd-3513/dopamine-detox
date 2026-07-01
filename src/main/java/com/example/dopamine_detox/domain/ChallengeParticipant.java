a563cd5a3c70120746c3d3adb1ee3b34581f99dd
dnjswldnd-3513 <zheld3513@gmail.com>
2026-07-01 21:53:08 +0900

    feat: ChallengeParticipant Entity class 구조 생성

--- src/main/java/com/example/dopamine_detox/domain/ChallengeParticipant.java
+++ src/main/java/com/example/dopamine_detox/domain/ChallengeParticipant.java
@@ -1,4 +1,35 @@
 package com.example.dopamine_detox.domain;
 
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class ChallengeParticipant {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "challenge_id")
+    private Challenge challenge;
+
+    @Column(nullable = false)
+    private boolean isCompleted = false;
+
+    @Builder
+    public ChallengeParticipant(Member member, Challenge challenge, boolean isCompleted) {
+        this.member = member;
+        this.challenge = challenge;
+        this.isCompleted = isCompleted;
+    }
 }
