f575a84cecbbbdd1cb72b57b62a940e9fa673d34
dnjswldnd-3513 <zheld3513@gmail.com>
2026-07-01 18:25:44 +0900

    feat: Score Entity class 구조 생성

--- src/main/java/com/example/dopamine_detox/domain/Score.java
+++ src/main/java/com/example/dopamine_detox/domain/Score.java
@@ -1,4 +1,40 @@
 package com.example.dopamine_detox.domain;
 
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Score {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @Column(nullable = false)
+    private int point;
+
+    @Column(nullable = false)
+    private String reason;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    @Builder
+    public Score(Member member, int point, String reason, LocalDateTime createdAt) {
+        this.member = member;
+        this.point = point;
+        this.reason = reason;
+        this.createdAt = createdAt;
+    }
 }
