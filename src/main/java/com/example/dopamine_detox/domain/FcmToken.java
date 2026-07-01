83201a9fb06aa493a8db36c09b32a053e2f714a4
dnjswldnd-3513 <zheld3513@gmail.com>
2026-07-01 18:19:39 +0900

    feat: FcmToken Entity class 구조 생성

--- src/main/java/com/example/dopamine_detox/domain/FcmToken.java
+++ src/main/java/com/example/dopamine_detox/domain/FcmToken.java
@@ -1,4 +1,30 @@
 package com.example.dopamine_detox.domain;
 
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class FcmToken {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @Column(unique = true)
+    private String token;
+
+    @Builder
+    public FcmToken(Member member, String token) {
+        this.member = member;
+        this.token = token;
+    }
 }
